ec2fd99d66b54c3897997c2c73db6833c9d00b64
Dominika K <kukisdominika@gmail.com>
2019-05-16 22:51:14 +0200

    CRUD

---
+++ EventApp/Controllers/EventController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EventApp.Models;
+
+namespace EventApp.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Event
+        public ActionResult Index()
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.EventTable.ToList());
+            }
+                
+        }
+
+        // GET: Event/Details/5
+        public ActionResult Details(int id)
+        {
+
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.EventTable.Where(x=> x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Event/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Event/Create
+        [HttpPost]
+        public ActionResult Create(EventTable eventTable)
+        {
+
+            using (DbModels dbModels = new DbModels())
+
+            {
+                dbModels.EventTable.Add(eventTable);
+                dbModels.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Event/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.EventTable.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Event/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, EventTable eventTable)
+        {
+            try
+            {
+                using (DbModels dbModels = new DbModels())
+                {
+                    dbModels.Entry(eventTable).State = EntityState.Modified;
+                    dbModels.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Event/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.EventTable.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Event/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, EventTable eventTable)
+        {
+            try
+            {
+                using (DbModels dbModels = new DbModels())
+                {
+                    eventTable = dbModels.EventTable.Where(x=>x.Id==id).FirstOrDefault();
+                    dbModels.EventTable.Remove(eventTable);
+                    dbModels.SaveChanges();
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EventApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventApp.Startup))]
+namespace EventApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
